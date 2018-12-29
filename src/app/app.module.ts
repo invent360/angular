9d631e57b91b74b98f4b95c359239f2d44778f6c
invent360 <46233704+invent360@users.noreply.github.com>
2018-12-29 15:14:32 +0000

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AccordionModule } from 'primeng/components/accordion/accordion';
 import { PanelModule } from 'primeng/components/panel/panel';
 import { ButtonModule } from 'primeng/components/button/button';
-import { RadioButtonModule } from 'primeng/components/radioButton/radioButton';
+//import { RadioButtonModule } from 'primeng/components/radioButton/radioButton';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
